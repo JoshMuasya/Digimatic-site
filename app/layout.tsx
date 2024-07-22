63353c5d84795baffe5a9dff9a1c271c0b69e0c2
JoshMuasya <muasyajoshua07@gmail.com>
2024-07-22 08:42:36 +0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Digimatic Marketers",
+  description: "Tech Company",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
